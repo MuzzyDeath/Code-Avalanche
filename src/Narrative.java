23667616fe7d14c92d704c18dcdf31b76df0ab6e
Cooper Riley <c_riley9@u.pacific.edu>
2019-10-23 22:01:00 -0700

    Figured out how to use scanners to print the lines we want.

--- src/Narrative.java
+++ src/Narrative.java
@@ -3,13 +3,26 @@ import java.io.FileNotFoundException;
 import java.util.Scanner; 
 
 public class Narrative {
-	public static void main(String[] args) throws FileNotFoundException { 
-		File file = new File("/Users/djcriley/git/group-project-code-avalanche/Text Files/NPC"); 
-		Scanner sc = new Scanner(file); 
+	public static void main(String[] args) {
+        
+		// Create an instance of File for data.txt file.
 		
-		// we just need to use \\Z as delimiter 
-		sc.useDelimiter("\\Z"); 
-		
-		System.out.println(sc.next()); 
-	} 
+        File file = new File("/Users/djcriley/git/group-project-code-avalanche/Text Files/NPC");
+        try {
+            // Create a new Scanner object which will read the data
+            // from the file passed in. To check if there are more 
+            // line to read from it we check by calling the 
+            // scanner.hasNextLine() method. We then read line one 
+            // by one till all lines is read.
+            Scanner scanner = new Scanner(file);
+            
+            for (int i = 0; i < 2; i++)
+            {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
