43fc59daa77514fa20dbf0c968cfe08d3d615c95
Nitin <n_pinnamaneni@u.pacific.edu>
2019-10-25 22:45:40 -0700

    Wrote the getCharacter and characterOnSpace methods

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,24 @@
 import starter.Space;
+import starter.Character;
 
 public class Board {
 	private Character[][] board;
 	private int numRows;
 	private int numCols;
 	private Space start;
+	
+	public Character getCharacter(Space space)
+	{
+		return new Character(space.getRow(), space.getCol());
+	}
+	
+	public boolean characterOnSpace(Space space)
+	{
+		boolean returnValue = false;
+		if(space != null)
+		{
+			returnValue = true;
+		}
+		return returnValue;
+	}
 }
