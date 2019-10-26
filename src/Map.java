201c8c82c81515df84cd2362ebaea55f770b3a02
Nitin <n_pinnamaneni@u.pacific.edu>
2019-10-26 11:09:05 -0700

    Created Map class with instance variables

---
+++ src/Map.java
@@ -0,0 +1,14 @@
+import starter.Space;
+
+public class Map {
+	private Board board;
+	private Space nextMap;
+	private int numMoves;
+	
+	public int getNumMoves()
+	{
+		return numMoves;
+	}
+	
+	
+}
